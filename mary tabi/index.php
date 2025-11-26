919cc597b18b75dc0e9428bf1f39e0fdf722f262
marytabi <tabi.tanyi@student.nhlstenden.com>
2025-11-26 23:54:04 +0100

    commit changes in index.php

--- mary tabi/index.php
+++ mary tabi/index.php
@@ -30,7 +30,7 @@ unset($lang_options[$current_lang]);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Wecome To Cameroon</title>
     <link rel="stylesheet" href="index.css">
 </head>
 <body>
