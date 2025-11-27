fd3d606c555c008d9a42b105afeeb9e6c8d47ba4
KristianEndrev <kristian.endrev@student.nhlstenden.com>
2025-11-27 16:03:33 +0100

    Added mountain section and a new font Oswald

--- nature.php
+++ nature.php
@@ -21,7 +21,28 @@
             </div>
         </div>
         <h1 class="top-main-text">Explore the rich colors and diverse scenery that make Bulgaria unique.</h1>
-
+        <section class="nature">
+            <h1 class="mountains">MOUNTAINS</h1>
+                <div class="horizontal-line"></div>
+        <div class="musala-white-box">
+                <img src="images/NATURE/Musala.jpg" alt="">
+                <p>“At 2,925 meters, Musala peak rises above the Balkans, offering unforgettable views for every hiker.”</p>
+        </div>
+            <div class="top-peak">
+                <img src="images/NATURE/Circle-VitoshaPeak.png" alt="Vitosha peak">
+                <p>Vitosha peak</p>
+            </div>
+        <div class="bottom-peaks">
+            <div class="left-peak">
+                <img src="images/NATURE/Circle-BotevPeak.png" alt="Vitosha peak">
+                <p>Botev peak</p>
+            </div>
+            <div class="right-peak">
+                <img src="images/NATURE/Circle-MalyovicaPeak.png" alt="Vitosha peak">
+                <p>Malyovica peak</p>
+            </div>
+        </div>
+        </section>
      </main>
      <?php include 'components/footer.html'?>
 </body>
